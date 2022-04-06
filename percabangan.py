01ee830330c2f27ded0c327a5aba111c309933a4
Muhammad Fauzan <90245924+Fauzan19M@users.noreply.github.com>
2022-04-07 00:28:10 +0700

    Menambahkan String dan Percabangan

--- percabangan.py
+++ percabangan.py
@@ -1,8 +1,11 @@
 # mencari nilai ganjil atau genap
+print("Mencari Nilai Ganjil atau Genap ")
 
 nilai = input(int("Masukkan Nilai :"))
 if nilai %2==0:
   print("Bilangan Ganjil")
+  if nilai == 3:
+    continue
  else:
   print("Bilangan Genap")
   
